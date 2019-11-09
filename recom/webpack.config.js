9a4412401970bab66a622bccb7ffcabad239f0e1
mohammad-khamaiseh-2 <m.khamaiseh2@student.aaup.edu>
2019-11-09 14:34:45 +0200

    Fix file struture

--- recom/webpack.config.js
+++ recom/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-var SRC_DIR = path.join(__dirname, "/src/index.jsx");
+var SRC_DIR = path.join(__dirname, "/client/index.jsx");
 var DIST_DIR = path.join(__dirname, "/public/");
 module.exports = {
   entry: SRC_DIR,
