460e51ba14e9308a9a42b913e7965bfba2d28f4a
mohammad-khamaiseh-2 <m.khamaiseh2@student.aaup.edu>
2019-11-09 17:25:20 +0200

    build proxy server

--- recom/server/index.js
+++ recom/server/index.js
@@ -8,10 +8,6 @@ var app = express();
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/items", function(req, res) {
-  console.log(req.body);
-  res.end("heeeeeeeeeey");
-});
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "../public"));
 });
