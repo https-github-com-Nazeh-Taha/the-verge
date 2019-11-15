96e6bf163233afa15aaa04fdfb6b2ad17da82538
RBK-30 <rbk-30@RBK-30s-Mac-mini.local>
2019-11-15 15:20:03 +0200

    comments ready

---
+++ comments/client/components/Comment.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+
+    // };
+
+  }
+
+  render(){
+    return(
+      <div className = 'commentItem'>
+
+				<div className = 'commentItemHeader'>
+					<img className = 'autherAvatar' src = {this.props.comment.profilePic}/>
+					<h2 className='autherName'>{this.props.comment.autherName}</h2>
+				</div>
+        
+        
+        <p className = 'commentbody'>{this.props.comment.body}</p>
+				<p>Posted on 	<a>{this.props.comment.createdAt}</a> </p>
+
+      </div>
+    )
+  }
+}
+
+export default Comment;
+
+
--- comments/client/index.jsx
+++ comments/client/index.jsx
@@ -1,20 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import $ from "jquery";
-import List from "./components/List.jsx";
+// import Comment from './components/Comment.jsx';
+// import CommentForm from "./components/CommentForm.jsx";
+// import CommentsList from "./components/CommentsList.jsx";
+// import data from '../dummyData.js';
+
+
 import "../public/style.css";
-class Article extends React.Component {
+import CommentsList from "./components/CommentsList.jsx";
+
+class CommentsModule extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: []
+    };
+
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+
+
+
+  getData(){
+    console.log(this.state.data);
+    var that = this;
+    $.ajax({
+      type: 'GET',
+      url:'/comments',
+      success: function(res) {
+        //var myData = JSON.stringify(res);
+        that.updateStatus(res);
+        console.log(that.state.data);
+
+      }
+
+
+    });
+  }
+
+  updateStatus(myData) {
+    this.setState({data: myData});
   }
   render() {
     return (
-      <div>
-        <h1>Hello from :</h1>
-        <List />
+      
+      <div className = 'mainDiv'>
+        
+
+        <div className = 'mainSection'>
+          <div className = 'commentsHeader'>
+            <h1 className = 'commentsCount'>THERE ARE {this.state.data.length} COMMENTS</h1>
+          </div>
+        
+          <div className = 'commentslist'>
+            <CommentsList  comments = {this.state.data} />
+          </div>
+        </div>
+
       </div>
     );
   }
 }
-ReactDOM.render(<Article />, document.getElementById("comments"));
+ReactDOM.render(<CommentsModule />, document.getElementById("comments"));
